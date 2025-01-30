b65844d05650e222d19405702f348c8935b590a4
Hanseooo <amoguishans@gmail.com>
2025-01-30 23:29:03 +0800

    css backdrop filter

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 function Navigation({ onChangeView }) {
 
     return(
-        <div className='mb-2 d-flex justify-content-between border-bottom p-2 py-2 sticky-top mt-2'>
+        <div id='navContainer' className='mb-2 d-flex justify-content-between border-bottom p-2 py-2 sticky-top mt-2'>
         <a className='fw-semibold text-decoration-none text-light fs-5 px-2' href='#'>World Chat</a>
         <div className=''>
         <button onClick={() => onChangeView('home')} className='btn btn-link text-light text-decoration-none navBtn'>Home</button>
